3815f1c388bea84cf0e2b8503d453f540e7ffe7e
Diogo Castro Silva <32181388+DiogoCastroSilva@users.noreply.github.com>
2025-03-15 16:29:02 +0000

    feat(next-level-food): Create the base for the project

---
+++ next-level-food/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main>
+      <Image
+        src="/logo.png"
+        alt="A server surrounded by magic sparkles."
+        width={180}
+        height={38}
+        priority
+      />
+      <h1>Welcome to this NextJS Course!</h1>
+      <p>🔥 Let&apos;s get started! 🔥</p>
+    </main>
+  );
+}
