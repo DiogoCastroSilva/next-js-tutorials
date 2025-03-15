0ad43b8e2345adb64d81de004ef10665d63784b4
Diogo Castro Silva <32181388+DiogoCastroSilva@users.noreply.github.com>
2025-03-15 20:18:12 +0000

    feat(header): Add the header background to the header component

--- next-level-food/app/components/main-header/main-header.tsx
+++ next-level-food/app/components/main-header/main-header.tsx
@@ -1,29 +1,34 @@
+import { Fragment } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
 import Logo from '@/assets/logo.png';
 import { HOME, MEALS, COMMUNITY } from '@/app/configs/routes';
+import MainHeaderBackground from '@/app/components/main-header-background/main-header-background';
 
 import styles from './main-header.module.css';
 
 const MainHeader = () => {
   return (
-    <header className={styles.header}>
-      <Link className={styles.logo} href={HOME}>
-        <Image src={Logo} alt="Logo - A plate with food on it" priority />
-        Next Level Food
-      </Link>
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href={MEALS}>Browse Meals</Link>
-          </li>
-          <li>
-            <Link href={COMMUNITY}>Foodies Cummunity</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <Fragment>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link className={styles.logo} href={HOME}>
+          <Image src={Logo} alt="Logo - A plate with food on it" priority />
+          Next Level Food
+        </Link>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href={MEALS}>Browse Meals</Link>
+            </li>
+            <li>
+              <Link href={COMMUNITY}>Foodies Cummunity</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </Fragment>
   );
 };
 
