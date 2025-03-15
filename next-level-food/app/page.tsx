cdc56f3ba6297cbda724f0dfe3205d770fcdd320
Diogo Castro Silva <32181388+DiogoCastroSilva@users.noreply.github.com>
2025-03-15 19:33:00 +0000

    feat(home):Update the main page and layout

--- next-level-food/app/page.tsx
+++ next-level-food/app/page.tsx
@@ -1,21 +1,9 @@
-import Image from 'next/image';
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <main>
-      <Image
-        src="/logo.png"
-        alt="A server surrounded by magic sparkles."
-        width={180}
-        height={38}
-        priority
-      />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <p>
-        <Link href="/about">About Us</Link>
-      </p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
