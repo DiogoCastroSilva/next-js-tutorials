852137bd51ee3d13c1ab2e98aa5a8f62a597aa43
Diogo Castro Silva <32181388+DiogoCastroSilva@users.noreply.github.com>
2025-03-16 16:06:28 +0000

    feat(community): Improve community content by moving items to a object

--- next-level-food/app/community/page.tsx
+++ next-level-food/app/community/page.tsx
@@ -1,9 +1,6 @@
 import Image from 'next/image';
 
-import mealIcon from '@/assets/icons/meal.png';
-import communityIcon from '@/assets/icons/community.png';
-import eventsIcon from '@/assets/icons/events.png';
-
+import { perks } from './data';
 import classes from './page.module.css';
 
 export default function Community() {
@@ -17,23 +14,13 @@ export default function Community() {
       </header>
       <main className={classes.main}>
         <h2>Community Perks</h2>
-
         <ul className={classes.perks}>
-          <li>
-            <Image src={mealIcon} alt="A delicious meal" />
-            <p>Share & discover recipes</p>
-          </li>
-          <li>
-            <Image src={communityIcon} alt="A crowd of people, cooking" />
-            <p>Find new friends & like-minded people</p>
-          </li>
-          <li>
-            <Image
-              src={eventsIcon}
-              alt="A crowd of people at a cooking event"
-            />
-            <p>Participate in exclusive events</p>
-          </li>
+          {perks.map((perk) => (
+            <li key={perk.name}>
+              <Image src={perk.icon} alt={perk.name} />
+              <p>{perk.name}</p>
+            </li>
+          ))}
         </ul>
       </main>
     </>
