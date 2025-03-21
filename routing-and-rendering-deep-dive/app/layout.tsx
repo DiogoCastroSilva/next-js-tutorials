713694d5cd0f812fc8abc4641871bbd2cb830505
Diogo Castro Silva <32181388+DiogoCastroSilva@users.noreply.github.com>
2025-03-21 12:50:04 +0000

    feat(routing-rendering): Create the new NextJs project

---
+++ routing-and-rendering-deep-dive/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next';
+
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Routing & Rendering: Deep Dive',
+  description: 'Learn how to route to different pages.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
