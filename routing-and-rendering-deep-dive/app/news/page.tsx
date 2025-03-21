ab182b0fd4892d1ed4bca494a99a284bdb20efc6
Diogo Castro Silva <32181388+DiogoCastroSilva@users.noreply.github.com>
2025-03-21 18:35:18 +0000

    feat(news): Create the news page by adding a list of news

---
+++ routing-and-rendering-deep-dive/app/news/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from 'next';
+
+import NewsList from '@/app/components/news-list/news-list';
+
+import styles from './page.module.css';
+
+export const metadata: Metadata = {
+  title: 'News',
+  description: 'Get the latest news from around the world.',
+};
+
+export default async function NewsPage() {
+  return (
+    <main className={styles.container}>
+      <NewsList />
+    </main>
+  );
+}
--- routing-and-rendering-deep-dive/next.config.ts
+++ routing-and-rendering-deep-dive/next.config.ts
@@ -1,7 +1,45 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mars.nasa.gov',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.nasa.gov',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nasaspaceflight.com',
+        port: '',
+        pathname: '/wp-content/uploads/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'science.nasa.gov',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.science.nasa.gov',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
