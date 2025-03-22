1b3d4b42857e4adc3b8ba2d4e1e86c6743d7187d
Diogo Castro Silva <32181388+DiogoCastroSilva@users.noreply.github.com>
2025-03-22 17:56:48 +0000

    feat(archive): Update API routes and add images security

--- routing-and-rendering-deep-dive/next.config.ts
+++ routing-and-rendering-deep-dive/next.config.ts
@@ -1,22 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'mars.nasa.gov',
-        port: '',
-        pathname: '/**',
-        search: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.nasa.gov',
-        port: '',
-        pathname: '/**',
-        search: '',
-      },
       {
         protocol: 'https',
         hostname: 'nasaspaceflight.com',
@@ -26,14 +12,84 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'science.nasa.gov',
+        hostname: 'www.nasaspaceflight.com',
+        port: '',
+        pathname: '/wp-content/uploads/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.science.nasa.gov',
         port: '',
         pathname: '/**',
         search: '',
       },
       {
         protocol: 'https',
-        hostname: 'assets.science.nasa.gov',
+        hostname: 'cdn.arstechnica.net',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.elonx.net',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.teslarati.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.tlpnetwork.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.cnbcfm.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'http',
+        hostname: 'spaceflightnow.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'spaceflightnow.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.spaceflightnow.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i0.wp.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mk0spaceflightnoa02a.kinstacdn.com',
         port: '',
         pathname: '/**',
         search: '',
