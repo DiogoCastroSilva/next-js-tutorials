1e95b92f7c7911018948db23f8e0697cafb92d1e
Diogo Castro Silva <32181388+DiogoCastroSilva@users.noreply.github.com>
2025-04-23 15:56:13 +0100

    feat(post-item): use custom markdown components for code

--- next-blog/components/posts/post-details/post-details.tsx
+++ next-blog/components/posts/post-details/post-details.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import ReactMarkdown, { Components } from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import PostHeader from './post-header';
 import styles from './post-details.module.css';
@@ -34,6 +36,16 @@ export default function PostDetails({ title, image, content }: IPost) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className?.split('-')[1];
+
+      return (
+        <SyntaxHighlighter style={atomDark} language={language}>
+          {children as string}
+        </SyntaxHighlighter>
+      );
     },
   };
 
